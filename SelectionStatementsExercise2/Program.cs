6eec113a173eaccc1de1fbd1ee05501f5001b4bd
Randall Smith <rdsmith1761@gmail.com>
2023-01-10 07:38:13 -0500

    exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Runtime;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    {
+                        Console.WriteLine("Math is hard");
+                        break;
+                    }
+                case "science":
+                    {
+                        Console.WriteLine("Hello Einstein!");
+                        break;
+                    }
+                case "gym":
+                    {
+                        Console.WriteLine("Everyone loves to run");
+                        break;
+                    }
+                case "history":
+                    {
+                        Console.WriteLine("We are doomed to repeat it");
+                        break;
+                    }
+                case "art":
+                    {
+                        Console.WriteLine("Are you the next Van Gogh?");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Never heard of it.");
+                        break;
+                    }
+                   
+            }
+                
         }
     }
 }
